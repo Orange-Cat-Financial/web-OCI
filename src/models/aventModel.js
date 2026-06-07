bad0e280463c86e56810bc8a20b3a3740ccbd24f
Vitor Canquerini <titolucas1616@gmail.com>
2026-06-07 14:37:13 -0300

    fix

--- src/models/aventModel.js
+++ src/models/aventModel.js
@@ -1,24 +1,24 @@
 var database = require("../database/config");
 
-function listar() {
-    console.log("ACESSEI O aventuras  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
-    var instrucaoSql = `
-        SELECT 
-            a.id AS idAventuras,
-            a.titulo,
-            a.descricao,
-            a.fkUsuario,
-            u.id AS idUsuario,
-            u.nome,
-            u.email,
-            u.senha
-        FROM aventuras a
-            INNER JOIN usuario u
-                ON a.fkUsuario = u.id;
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
+// function listar() {
+//     console.log("ACESSEI O aventuras  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+//     var instrucaoSql = `
+//         SELECT 
+//             a.id AS idAventuras,
+//             a.titulo,
+//             a.descricao,
+//             a.fkUsuario,
+//             u.id AS idUsuario,
+//             u.nome,
+//             u.email,
+//             u.senha
+//         FROM aventuras a
+//             INNER JOIN usuario u
+//                 ON a.fkUsuario = u.id;
+//     `;
+//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
+//     return database.executar(instrucaoSql);
+// }
 
 function pesquisarDescricao(texto) {
     console.log("ACESSEI O aventuras MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function pesquisarDescricao()");
